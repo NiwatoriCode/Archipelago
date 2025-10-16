0f6d8878fa15d24f1880d873e92552c5075a16aa
NiwatoriCode <213150313+NiwatoriCode@users.noreply.github.com>
2025-10-16 03:58:03 -0400

    sadv2: Added basic item definitions
    Only Sonic is added for now. The other characters will be added when I get a working build with just Sonic.

--- worlds/sadv2/Items.py
+++ worlds/sadv2/Items.py
@@ -1,1 +1,59 @@
+import typing
+
 from BaseClasses import Item, ItemClassification
+
+class ItemData(typing.NamedTuple):
+    code: typing.Optional[int]
+    progression: bool
+    event: bool = False
+
+class SADV2Item(Item):
+    game: str = "Sonic Advance 2"
+
+character_table = {
+    "Sonic Rescued": ItemData(100, True)
+}
+
+zone_table = {
+    "Leaf Forest": ItemData(200, True),
+    "Hot Crater": ItemData(201, True),
+    "Music Plant": ItemData(202, True),
+    "Ice Paradice": ItemData(203, True),
+    "Sky Canyon": ItemData(204, True),
+    "Techno Base": ItemData(205, True),
+    "Egg Utopia": ItemData(206, True)
+}
+
+emerald_table = {
+    "Red Chaos Emerald": ItemData(300, True),
+    "Blue Chaos Emerald": ItemData(301, True),
+    "Yellow Chaos Emerald": ItemData(302, True),
+    "Green Chaos Emerald": ItemData(303, True),
+    "White Chaos Emerald": ItemData(304, True),
+    "Cyan Chaos Emerald": ItemData(305, True),
+    "Purple Chaos Emerald": ItemData(306, True)
+}
+
+filler_table = {
+    "Cheat Code to Unlock Shadow": ItemData(400, False),
+    "The Other Half of the Moon": ItemData(401, False),
+    "Love Letter From Vector": ItemData(402, False),
+    "Computer Room": ItemData(403, False),
+    "Huge Chao Garden": ItemData(404, False),
+    "Scratch and Grounder": ItemData(405, False),
+    "Bocoe and Decoe": ItemData(406, False),
+    "Orbot and Cubot": ItemData(407, False),
+    "Phantom Ruby": ItemData(408, False),
+    "Every Single Drop of All You've Got": ItemData(409, False)
+}
+
+event_table = {
+    "XX Coordinates 1": ItemData(None, True, True),
+    "XX Coordinates 2": ItemData(None, True, True),
+    "XX Coordinates 3": ItemData(None, True, True),
+    "XX Coordinates 4": ItemData(None, True, True),
+    "XX Coordinates 5": ItemData(None, True, True),
+    "XX Coordinates 6": ItemData(None, True, True),
+    "XX Coordinates 7": ItemData(None, True, True),
+    "Vanilla Rescued": ItemData(None, True, True)
+}
